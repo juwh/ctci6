270e197dc0e61650ddedfd7b5a33d951a2277c1d
William Ju <william.h.ju@vanderbilt.edu>
2021-07-03 22:26:03 +0000

    add TrieNode under ctcilib

--- lib/ctcilib/TrieNode.cpp
+++ lib/ctcilib/TrieNode.cpp
@@ -1,74 +1,41 @@
-// package CtCILibrary;
+#include "TrieNode.h"
 
-// import java.util.HashMap;
+using namespace ctcilib;
 
-// /* One node in the trie. Most of the logic of the trie is implemented
-//  * in this class.
-//  */
-// public class TrieNode {
-//     /* The children of this node in the trie.*/
-//     private HashMap<Character, TrieNode> children;
-//     private boolean terminates = false;
+TrieNode::TrieNode() {}
 
-//     // The character stored in this node as data.
-//     private char character;	
+TrieNode::TrieNode(char character) : m_character{character} {}
 
-// 	/* Constructs a trie node and stores this character as the node's value.
-// 	 * Initializes the list of child nodes of this node to an empty hash map. */
-//     public TrieNode() {
-//     	children = new HashMap<Character, TrieNode>();
-//     }
+char TrieNode::get_char() {
+    return m_character;
+}
 
-//     /* Constructs a trie node and stores in the node the char passed in
-//      * as the argument. Initializes the list of child nodes of this
-//      * node to an empty hash map.
-//      */
-//     public TrieNode(char character) {
-//         this();
-//         this.character = character;
-//     }
+void TrieNode::add_word(std::string word) {
+    if (!word.empty()) {
+        char first_char = word.at(0);
 
-//     /* Returns the character data stored in this node. */
-//     public char getChar() {
-//         return character;
-//     }
+        TrieNode* child = get_child(first_char);
+        if (!child) {
+            child = new TrieNode(first_char);
+            children.insert({first_char, child});
+        } 
 
-//     /* Add this word to the trie, and recursively create the child
-//      * nodes. */
-//     public void addWord(String word) {
-//     	if (word == null || word.isEmpty()) {
-//     		return;
-//     	}
-    	
-//         char firstChar = word.charAt(0);
+        if (word.length() > 1) {
+            child->add_word(word.substr(1));
+        } else {
+            child->set_terminates(true);
+        };
+    }
+}
 
-//         TrieNode child = getChild(firstChar);
-//         if (child == null) {
-//             child = new TrieNode(firstChar);
-//             children.put(firstChar, child);
-//         } 
+TrieNode* TrieNode::get_child(char c) {
+    return children.at(c);
+}
 
-//         if (word.length() > 1) {
-//             child.addWord(word.substring(1));
-//         } else {
-//         	child.setTerminates(true);
-//         }
-//     }
+bool TrieNode::terminates() {
+    return m_terminates;
+}
 
-//     /* Find a child node of this node that has the char argument as its
-//      * data. Return null if no such child node is present in the trie.
-//      */
-//     public TrieNode getChild(char c) {
-//     	return children.get(c);
-//     }
-
-//     /* Returns whether this node represents the end of a complete word. */
-//     public boolean terminates() {
-//     	return terminates;
-//     }
-    
-//     /* Set whether this node is the end of a complete word.*/
-//     public void setTerminates(boolean t) {
-//     	terminates = t;
-//     }
-// }
+void TrieNode::set_terminates(bool t) {
+    m_terminates = t;
+}
---
+++ lib/ctcilib/TrieNode.h
@@ -0,0 +1,50 @@
+#ifndef TRIENODE_H_
+#define TRIENODE_H_
+
+#include <unordered_map>
+
+namespace ctcilib {
+    /* One node in the trie. Most of the logic of the trie is implemented
+    * in this class.
+    */
+    class TrieNode {
+        private:
+            /* The children of this node in the trie.*/
+            std::unordered_map<char, TrieNode*> children;
+            bool m_terminates {false};
+
+            // The character stored in this node as data.
+            char m_character;	
+
+        public:
+            /* Constructs a trie node and stores this character as the node's value.
+            * Initializes the list of child nodes of this node to an empty hash map. */
+            TrieNode();
+
+            /* Constructs a trie node and stores in the node the char passed in
+            * as the argument. Initializes the list of child nodes of this
+            * node to an empty hash map.
+            */
+            TrieNode(char character);
+
+            /* Returns the character data stored in this node. */
+            char get_char();
+
+            /* Add this word to the trie, and recursively create the child
+            * nodes. */
+
+            void add_word(std::string word);
+            /* Find a child node of this node that has the char argument as its
+            * data. Return null if no such child node is present in the trie.
+            */
+            TrieNode* get_child(char c);
+
+            /* Returns whether this node represents the end of a complete word. */
+            bool terminates();
+            
+            /* Set whether this node is the end of a complete word.*/
+            void set_terminates(bool t);
+    };
+}
+
+#endif // TRIENODE_H_
