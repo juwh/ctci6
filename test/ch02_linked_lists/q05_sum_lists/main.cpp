9bccf890bab97a7e87731da6f19601934f0f97fb
William Ju <williamhju@gmail.com>
2021-07-18 18:14:29 -0400

    solutions through palindrome

--- test/ch02_linked_lists/q05_sum_lists/main.cpp
+++ test/ch02_linked_lists/q05_sum_lists/main.cpp
@@ -40,3 +40,179 @@ represents the 10's place. What if you made them the same length? Is there a way
 modify the linked list to do that, without changing the value it represents?
 */
 #pragma endregion
+
+#include <ctcilib/LinkedListNode.h>
+
+#include <iostream>
+#include <tuple>
+
+ctcilib::LinkedListNode<int> SumLists(const ctcilib::LinkedListNode<int>& l1, const ctcilib::LinkedListNode<int>& l2) {
+    const ctcilib::LinkedListNode<int>* cur_l1{&l1};
+    const ctcilib::LinkedListNode<int>* cur_l2{&l2};
+    ctcilib::LinkedListNode<int>* out = new ctcilib::LinkedListNode<int>(0);
+    int carry{0};
+
+    while (cur_l1 || cur_l2) {
+        int val_l1{cur_l1 ? cur_l1->data_ : 0};
+        int val_l2{cur_l2 ? cur_l2->data_ : 0};
+        int sum = val_l1 + val_l2 + carry;
+        int digit = sum%10;
+        carry = sum/10;
+        out->push_back(digit);
+
+        cur_l1 = cur_l1 ? cur_l1->next_: nullptr;
+        cur_l2 = cur_l2 ? cur_l2->next_: nullptr;
+    }
+
+    if (carry) {
+        out->push_back(carry);
+    }
+
+    // remove dummy head
+    if (out->next_) {
+        auto del_node{out};
+        out = out->next_;
+
+        // prep node for deletion
+        del_node->next_ = nullptr;
+        delete del_node;
+    }
+    return *out;
+}
+
+ctcilib::LinkedListNode<int> SumListsTwoPass(const ctcilib::LinkedListNode<int>& l1, const ctcilib::LinkedListNode<int>& l2) {
+    const ctcilib::LinkedListNode<int>* cur_l1{&l1};
+    const ctcilib::LinkedListNode<int>* cur_l2{&l2};
+    ctcilib::LinkedListNode<int>* out = new ctcilib::LinkedListNode<int>(0);
+
+    while (cur_l1 || cur_l2) {
+        int val_l1{cur_l1 ? cur_l1->data_ : 0};
+        int val_l2{cur_l2 ? cur_l2->data_ : 0};
+        int sum = val_l1 + val_l2;
+        out->push_back(sum);
+
+        cur_l1 = cur_l1 ? cur_l1->next_: nullptr;
+        cur_l2 = cur_l2 ? cur_l2->next_: nullptr;
+    }
+
+    ctcilib::LinkedListNode<int>* sec_pass{out};
+    int carry{0};
+    while (sec_pass) {
+        if (carry) {
+            sec_pass->data_ += carry;
+        }
+        if (sec_pass->data_ >= 10) {
+            carry = sec_pass->data_/10;
+            sec_pass->data_ %= 10;
+        }
+        sec_pass = sec_pass->next_;
+    }
+
+    if (carry) {
+        out->push_back(carry);
+    }
+
+    // remove dummy head
+    if (out->next_) {
+        auto del_node{out};
+        out = out->next_;
+
+        // prep node for deletion
+        del_node->next_ = nullptr;
+        delete del_node;
+    }
+    return *out;
+}
+
+ctcilib::LinkedListNode<int>* SumListsRecursive(const ctcilib::LinkedListNode<int>* l1, const ctcilib::LinkedListNode<int>* l2, int carry) {
+    if (!l1 && !l2 && !carry) {
+        return nullptr;
+    }
+
+    int val_l1{l1 ? l1->data_ : 0};
+    int val_l2{l2 ? l2->data_ : 0};
+    int sum = val_l1 + val_l2 + carry;
+    int next_carry = sum/10;
+
+    auto next_node = SumListsRecursive(l1 ? l1->next_: nullptr, l2 ? l2->next_: nullptr, next_carry);
+    return new ctcilib::LinkedListNode<int>(sum%10, next_node, nullptr);
+}
+
+ctcilib::LinkedListNode<int>* InsertBefore(ctcilib::LinkedListNode<int>* l, int data) {
+    return new ctcilib::LinkedListNode<int>(data, l, nullptr);
+}
+
+ctcilib::LinkedListNode<int>* PadList(ctcilib::LinkedListNode<int>* l, size_t padding) {
+    auto head = l;
+    for (size_t i = 0; i < padding; i++) {
+        head = InsertBefore(head, 0);
+    }
+    return head;
+}
+
+struct PartialSum {
+    ctcilib::LinkedListNode<int>* sum_{nullptr};
+    int carry_{0};
+};
+
+PartialSum SumListsForwardHelper(const ctcilib::LinkedListNode<int>* l1, const ctcilib::LinkedListNode<int>* l2) {
+    if (!l1 || !l2) {
+        return PartialSum();
+    }
+
+    PartialSum part_sum = SumListsForwardHelper(l1->next_, l2->next_);
+    int digit_sum = l1->data_ + l2->data_ + part_sum.carry_;
+    part_sum.sum_ = InsertBefore(part_sum.sum_, digit_sum%10);
+    part_sum.carry_ = digit_sum/10;
+    return part_sum;
+}
+
+ctcilib::LinkedListNode<int>* SumListsForward(const ctcilib::LinkedListNode<int>* l1, const ctcilib::LinkedListNode<int>* l2) {
+    ctcilib::LinkedListNode<int>* copy_l1 = new ctcilib::LinkedListNode<int>(*l1);
+    ctcilib::LinkedListNode<int>* copy_l2 = new ctcilib::LinkedListNode<int>(*l2);
+    size_t length_l1{0}, length_l2{0};
+
+    for (auto cur_l1 = l1; cur_l1; cur_l1 = cur_l1->next_, length_l1++) {}
+    for (auto cur_l2 = l2; cur_l2; cur_l2 = cur_l2->next_, length_l2++) {}
+    auto shorter = length_l1 > length_l2 ? PadList(copy_l2, length_l2 - length_l1) : PadList(copy_l1, length_l1 - length_l2);
+    auto longer = length_l1 > length_l2 ? l1 : l2;
+
+    PartialSum sum = SumListsForwardHelper(l1, l2);
+    auto out = sum.sum_;
+    if (sum.carry_) {out = InsertBefore(sum.sum_, sum.carry_);}
+    return out;
+}
+
+int LinkedListToInt(const ctcilib::LinkedListNode<int>& node) {
+    int value = 0;
+    if (node.next_ != nullptr) {
+        value = 10 * LinkedListToInt(*node.next_);
+    }
+    return value + node.data_;
+}	
+
+int main() {
+    ctcilib::LinkedListNode<int> lA1(9, nullptr, nullptr);
+    lA1.push_back(9);
+    lA1.push_back(9);
+    
+    ctcilib::LinkedListNode<int> lB1(1, nullptr, nullptr);
+    lB1.push_back(0);
+    lB1.push_back(0);
+    
+    //auto list3 = SumLists(lA1, lB1);
+    //auto list3 = SumListsTwoPass(lA1, lB1);
+    //auto list3 = *SumListsRecursive(&lA1, &lB1, 0);
+    auto list3 = *SumListsForward(&lA1, &lB1);
+    
+    std::cout << "  " << lA1.print_forward() << std::endl;	
+    std::cout << "+ " << lB1.print_forward() << std::endl;	
+    std::cout << "= " << list3.print_forward() << std::endl;			
+    
+    int l1 = LinkedListToInt(lA1);
+    int l2 = LinkedListToInt(lB1);
+    int l3 = LinkedListToInt(list3);
+    
+    std::cout << std::to_string(l1) << " + " << l2 << " = " << l3 << std::endl;
+    std::cout << std::to_string(l1) << " + " << l2 << " = " << (l1 + l2) << std::endl;	
+}
--- test/ch02_linked_lists/q06_palindrome/main.cpp
+++ test/ch02_linked_lists/q06_palindrome/main.cpp
@@ -23,3 +23,62 @@ check that a->x->middle->y->b is a palindrome?
 You can do this with a new class.
 */
 #pragma endregion
+
+#include <ctcilib/LinkedListNode.h>
+
+#include <iostream>
+#include <vector>
+
+bool IsPalindrome(const ctcilib::LinkedListNode<int>& head) {
+    return true;
+}
+
+bool IsPalindromeRecursive(const ctcilib::LinkedListNode<int>& head) {
+    return true;
+}
+
+int main() {
+    int max = 11;
+    for (int length = 1; length < max; length++) {
+        std::vector<ctcilib::LinkedListNode<int>> nodes(length);
+        for (int i = 0; i < length; i++) {
+            nodes[i] = ctcilib::LinkedListNode<int>(i >= length / 2 ? length - i - 1 : i, nullptr, nullptr);
+        }
+        
+        for (int i = 0; i < length; i++) {
+            if (i < length - 1) {
+                nodes[i].set_next(&nodes[i + 1]);
+            }
+            if (i > 0) {
+                nodes[i].set_previous(&nodes[i - 1]);
+            }
+        }
+        for (int i = -1; i < length; i++) {
+            if (i >= 0) {
+                nodes[i].data_++; // Ruin palindrome
+            }
+            
+            ctcilib::LinkedListNode<int> head = nodes[0];
+            std::cout << head.print_forward() << std::endl;
+            bool result_a = IsPalindrome(head);
+            bool result_b = IsPalindrome(head);
+            bool result_c = IsPalindromeRecursive(head);
+            std::cout << "A: " << result_a << std::endl;
+            std::cout << "B: " << result_b << std::endl;
+            std::cout << "C: " << result_c << std::endl;
+            if (result_a != result_b || result_b != result_c) {
+                std::cout << "ERROR" << std::endl;
+                length = max;
+                break;
+            }
+            if (i >= 0) {
+                nodes[i].data_--;
+            }
+        }
+        // clear node from end to head to avoid deleted node conflicts
+        for (int i = length - 2; i >= 0; i--) {
+            nodes.pop_back();
+            nodes[i].set_next(nullptr);
+        }
+    }
+}
